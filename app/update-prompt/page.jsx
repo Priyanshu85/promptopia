03a7535a18612d8121b503639bbec1ce4bb3e419
Priyanshu85 <pg7909290@gmail.com>
2024-03-31 16:53:34 +0530

    Full code push

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,6 @@
 "use client"
-import { useState,useEffect } from 'react'
+import { Suspense } from 'react';
+import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
@@ -7,7 +8,7 @@ import Form from '@components/Form';
 
 const UpdatePrompt = () => {
     const router = useRouter();
-    const {data:session} = useSession();
+    const { data: session } = useSession();
     const searchParams = useSearchParams();
     const promptId = searchParams.get('id');
     const [submitting, setSubmitting] = useState(false);
@@ -16,8 +17,8 @@ const UpdatePrompt = () => {
         tag: '',
     });
 
-    useEffect(()=>{
-        const getPromptDetails = async() => {
+    useEffect(() => {
+        const getPromptDetails = async () => {
             const response = await fetch(`/api/prompt/${promptId}`);
             const data = await response.json();
 
@@ -28,12 +29,12 @@ const UpdatePrompt = () => {
         }
 
         if (promptId) getPromptDetails();
-    },[promptId])
+    }, [promptId])
 
     const updatePrompt = async (e) => {
         e.preventDefault();
-            setSubmitting(true);
-            if(!promptId) return alert('Prompt ID not found');
+        setSubmitting(true);
+        if (!promptId) return alert('Prompt ID not found');
         try {
             const response = await fetch(`/api/prompt/${promptId}`, {
                 method: 'PATCH',
@@ -48,20 +49,21 @@ const UpdatePrompt = () => {
         } catch (error) {
             console.log(error);
         }
-        finally
-        {
+        finally {
             setSubmitting(false);
         }
     }
 
     return (
-        <Form
-            type="Edit"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+            <Form
+                type="Edit"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
     )
 }
 
